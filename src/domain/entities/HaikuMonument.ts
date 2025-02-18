de243ed198011cc61c86b748344c4c3fff982f3c
shikano <pegtar377512@gmail.com>
2025-02-18 17:53:42 +0900

    fix: idフィールド、createdAt、updatedAtを必須フィールドに変更

--- src/domain/entities/HaikuMonument.ts
+++ src/domain/entities/HaikuMonument.ts
@@ -3,7 +3,7 @@ import type { Source } from './Source';
 import type { Location } from './Location';
 
 export interface HaikuMonument {
-  id?: number;
+  id: number;
   text: string;
   authorId: number | null;
   author?: Author | null;
@@ -14,7 +14,8 @@ export interface HaikuMonument {
   location?: Location | null;
   commentary: string | null;
   imageUrl?: string | null;
-  createdAt?: string | null;
-  updatedAt?: string | null;
+  createdAt: string | null;
+  updatedAt: string | null;
 }
 
+export type CreateHaikuMonumentInput = Omit<Source, 'id' | 'createdAt' | 'updatedAt'>;
--- src/domain/entities/Source.ts
+++ src/domain/entities/Source.ts
@@ -1,10 +1,12 @@
 export interface Source {
-  id?: number;
+  id: number;
   title: string;
   author?: string | null;
   year?: number | null;
   url?: string | null;
   publisher?: string | null;
-  createdAt?: string | null;
-  updatedAt?: string | null;
+  createdAt: string | null;
+  updatedAt: string | null;
 }
+
+export type CreateSourceInput = Omit<Source, 'id' | 'createdAt' | 'updatedAt'>;
