d4328d730fd43fcbe2aa8daab2039030ac227be6
shikano <pegtar377512@gmail.com>
2025-02-19 13:34:47 +0900

    del: 開発および本番環境の設定ファイルを削除

--- src/config/development.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config({ path: ".env.development" });
-
-export default {
-  env: process.env.NODE_ENV || "development",
-  dbFile: process.env.DB_FILE || "./data/db.sqlite",
-  port: process.env.PORT || 3000,
-};
