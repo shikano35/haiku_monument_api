059b396089017515c4df0650339e3b9c9cb8e672
takemura <170946913+shikano35@users.noreply.github.com>
2025-05-11 12:21:56 +0900

    feat: Location, Poet, and Sourceレスポンスにエンティティタイプを追加

--- src/interfaces/dtos/LocationResponse.ts
+++ src/interfaces/dtos/LocationResponse.ts
@@ -3,7 +3,17 @@ export type LocationResponse = {
   prefecture: string;
   region: string | null;
   address: string | null;
+  name: string | null;
   latitude: number | null;
   longitude: number | null;
+};
+
+export type LocationEntityResponse = {
+  id: number;
+  prefecture: string;
+  region: string | null;
+  address: string | null;
   name: string | null;
+  latitude: number | null;
+  longitude: number | null;
 };
--- src/interfaces/dtos/PoetResponse.ts
+++ src/interfaces/dtos/PoetResponse.ts
@@ -7,3 +7,13 @@ export type PoetResponse = {
   created_at: string;
   updated_at: string;
 };
+
+export type PoetEntityResponse = {
+  id: number;
+  name: string;
+  biography: string | null;
+  links: string | null;
+  imageUrl: string | null;
+  createdAt: string | null;
+  updatedAt: string | null;
+};
